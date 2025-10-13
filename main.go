6be8eb8ea47cf4c6a4ed99cdc0aad4ca60d47462
Nikita Pozdeev <pozdeev.nikita@yandex.ru>
2025-10-13 23:44:45 +0300

    fix err two

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
-package main 
+package main
 
 import "fmt"
 
-func main(){
-	fmt.Println(hello())
+func main() {
+    fmt.Println(hello())
 }
 
-func hello()string{
-	return "Hello Go"
+func hello() string {
+    return "Hello Go" // эту строку исправили Go теперь не go
 }
